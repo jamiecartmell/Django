949bd1aa7d78e5c16578d14507c1d9566f9359b6
jamie cartmell <jamie@wordpressupdates.ie>
2024-02-08 13:05:09 +0000

    updateto allowed host

--- django_project/settings.py
+++ django_project/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 # DEBUG = True
 DEBUG = os.environ.get("DEBUG", "False").lower() == "true"
 
-ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS")
+ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS", "").split(",")
 
 
 # ALLOWED_HOSTS = []
