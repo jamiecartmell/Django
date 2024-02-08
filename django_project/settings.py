fd9bf08c0e9b5294093a2b3f059533358d2b0847
jamie cartmell <jamie@wordpressupdates.ie>
2024-02-08 12:42:09 +0000

    upload

--- django_project/settings.py
+++ django_project/settings.py
@@ -14,6 +14,7 @@ import dj_database_url
 import os
 from pathlib import Path
 
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -23,19 +24,16 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 # SECRET_KEY = "django-insecure-!d#tj0x83*9qt-r$*er-+ykz)9^^t*6nkvp2im)(!^xfdotac)"
-SECRET_KEY = os.environ.get("SECRET_KEY", "your_default_secret_key_here")
+SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
+
 # DEBUG = True
 DEBUG = os.environ.get("DEBUG", "False").lower() == "true"
 
-ALLOWED_HOSTS = [
-    host.strip()
-    for host in os.environ.get("ALLOWED_HOSTS", "").split(",")
-    if host.strip()
-]
+ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS").split("")
 
 
-# ALLOWED_HOSTS = ["workforce-app-pe3c.onrender.com"]
+# ALLOWED_HOSTS = []
 
 
 # Application definition
@@ -98,10 +96,15 @@ DATABASES = {
         "NAME": BASE_DIR / "db.sqlite3",
     }
 }
-database_url = os.environ.get("DATABASE_URL")
-DATABASES = {"default": dj_database_url.parse(database_url)}
 
-# postgres://workforce_database_user:8Z2XHRsgfF7eaXuz3Z9f79veQy5mSWys@dpg-cn29cced3nmc739bhvr0-a.frankfurt-postgres.render.com/workforce_database
+# DATABASES = {
+# "default": dj_database_url.parse(
+#      "postgres://workforce_database_user:8Z2XHRsgfF7eaXuz3Z9f79veQy5mSWys@dpg-cn29cced3nmc739bhvr0-a.frankfurt-postgres.render.com/workforce_database"
+#   )
+# }
+database_url = os.environ.get("DATABASE_URL")
+DATABASES["default"] = dj_database_url.parse(database_url)
+
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
 
