4b640a854fe41f1c27b073a97f59631e0c1364ab
jamie cartmell <jamie@wordpressupdates.ie>
2024-02-08 14:47:15 +0000

    update to static

--- django_project/settings.py
+++ django_project/settings.py
@@ -44,7 +44,7 @@ ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS")
 if ALLOWED_HOSTS:
     ALLOWED_HOSTS = [host.strip() for host in ALLOWED_HOSTS.split(",")]
 else:
-    ALLOWED_HOSTS = []
+    ALLOWED_HOSTS = ["127.0.0.1"]
 
 # Add 'django-workforce.onrender.com' to the list of allowed hosts
 ALLOWED_HOSTS.append("django-workforce.onrender.com")
@@ -153,7 +153,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = "/static/"
+STATIC_URL = "/staticfiles/"
 STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
 
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
